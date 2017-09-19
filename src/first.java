3fd722f53c7152fe7d1f0235aebfa394b14d7588
nanikoduri <nani15@stu.calumsva.edu>
2017-09-19 17:51:51 -0400

    added new code

---
+++ src/first.java
@@ -0,0 +1,16 @@
+
+public class first {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		System.out.println("value of c is"  + c);
+
+	}
+
+}
